b259d81024faccdde4efd2a0050824fad1929435
doraemon <bearclawr@163.com>
2020-03-25 11:03:11 +0800

    Initial commit

---
+++ Foster-companion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foster-companion
+//
+//  Created by Doraemon on 2020/3/25.
+//  Copyright © 2020 Doraemon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
